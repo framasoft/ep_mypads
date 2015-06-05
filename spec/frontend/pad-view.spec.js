9bbae96573305f372429ad0c9c7bf4e2ff48b0c0
Fabien Bourgeois <fabien@yaltik.com>
2015-06-05 17:13:31 +0200

    frontend pad view functional testing : missing file...

---
+++ spec/frontend/pad-view.spec.js
@@ -0,0 +1,94 @@
+/**
+*  # Pad view functional testing
+*
+*  ## License
+*
+*  Licensed to the Apache Software Foundation (ASF) under one
+*  or more contributor license agreements.  See the NOTICE file
+*  distributed with this work for additional information
+*  regarding copyright ownership.  The ASF licenses this file
+*  to you under the Apache License, Version 2.0 (the
+*  "License"); you may not use this file except in compliance
+*  with the License.  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing,
+*  software distributed under the License is distributed on an
+*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*  KIND, either express or implied.  See the License for the
+*  specific language governing permissions and limitations
+*  under the License.
+*
+*/
+
+module.exports = (function () {
+  'use strict';
+  // Dependencies
+  var common = require('./common.js');
+  var fill = common.helper.fill;
+
+  var view = {};
+
+
+  view.run = function (app) {
+
+    // Shared variables
+    var qfirst = function (sel) { return app.document.querySelector(sel); };
+
+    describe('pad view module testing', function () {
+
+      beforeAll(function (done) {
+        // Login and go to group view page
+        app.document.querySelector('header > nav a:first-child').click();
+        window.setTimeout(function () {
+          fill(app.document.querySelector('input[name=login]'), 'parker');
+          fill(app.document.querySelector('input[name=password]'),
+            'lovesKubiak');
+          app.document.querySelector('input[type=submit]').click();
+          window.setTimeout(function () {
+            app.document.querySelectorAll('a[href$=view]')[2].click();
+            window.setTimeout(function () {
+              qfirst('body > section > div p').click();
+              window.setTimeout(done, 100);
+            }, 200);
+          }, 200);
+        }, 200);
+      });
+
+      afterAll(function (done) {
+        app.document.querySelector('.icon-logout').parentNode.click();
+        window.setTimeout(function () {
+          app.document.querySelector('body > section p').click();
+          done();
+        }, 100);
+      });
+
+      describe('public group view and properties', function () {
+
+        beforeAll(function (done) {
+          qfirst('section.pad li span a').click();
+          window.setTimeout(done, 200);
+        });
+
+        it('should display properties as expected', function (done) {
+          var title = qfirst('section.group h2 span');
+          var group = qfirst('span.subtitle');
+          var iframe = qfirst('iframe');
+          window.setTimeout(function () {
+            expect(title.textContent).toBe('Pad Loving Annie');
+            expect(group.textContent).toMatch('memories');
+            expect(iframe.getAttribute('src')).not.toMatch('mypadspassword');
+            done();
+          }, 100);
+        });
+
+      });
+
+    });
+
+  };
+
+  return view;
+
+}).call(this);

framasoft/ep_mypads

commit e558bec1d373050fdb26e1cb88af45ef28b69bbb

Fix #105

frontend/js/configuration.js
@@ -32,7 +32,8 @@ module.exports = (function () {
   var auth = require('./auth.js');
 
   var config = {};
-  config.URLS = { BASE: '/mypads/api' };
+  config.URLS = { RAWBASE: window.location.pathname };
+  config.URLS.BASE = config.URLS.RAWBASE + 'api';
   config.URLS.CONF = config.URLS.BASE + '/configuration';
   config.URLS.AUTH = config.URLS.BASE + '/auth';
   config.URLS.LOGIN = config.URLS.AUTH + '/login';
@@ -45,6 +46,7 @@ module.exports = (function () {
   config.URLS.USERLIST = config.URLS.BASE + '/userlist';
   config.URLS.PASSRECOVER = config.URLS.BASE + '/passrecover';
   config.URLS.ACCOUNT_CONFIRMATION = config.URLS.BASE + '/accountconfirm';
+  config.URLS.CACHECHECK = config.URLS.BASE + '/cache/check';
   config.SERVER = m.prop();
   // default to en
   var USERLANG_DEFAULT = 'en';
@@ -61,7 +63,7 @@ module.exports = (function () {
   config.updateLang = function (key) {
     m.request({
       method: 'GET',
-      url: '/mypads/l10n/' + key + '.json'
+      url: config.URLS.RAWBASE + 'l10n/' + key + '.json'
     }).then(function (resp) {
       config.USERLANG = key;
       config.LANG = resp;

frontend/js/helpers/ready.js
@@ -28,6 +28,7 @@
 module.exports = (function () {
   // Dependencies
   var m = require('mithril');
+  var conf = require('../configuration.js');
 
   var ready = {};
 
@@ -40,7 +41,7 @@ module.exports = (function () {
     if (isInitialized) return;
     m.request({
       method: 'GET',
-      url: '/mypads/api/cache/check'
+      url: conf.URLS.CACHECHECK
     }).then(function (resp) {
       if (resp.userCacheReady) {
         var toHide = document.getElementById('hide-when-ready').classList;

frontend/js/modules/pad-view.js
@@ -151,7 +151,7 @@ module.exports = (function () {
   view.pad = function (c) {
     var p = (c.sendPass() ? '&mypadspassword=' + encode(c.password()) : '');
     var a = (auth.isAuthenticated() ? '&auth_token=' + auth.token() : '');
-    var link = '/p/' + c.pad._id + '?' + p + a;
+    var link = conf.URLS.RAWBASE.replace('mypads/', '') + 'p/' + c.pad._id + '?' + p + a;
     return [
       m('p.text-right', [
         m('a.btn.btn-default.expand-toggle', {

commit f3d58dda4a500626e2f0122a22a5d24555c77e77

Fix bug introduced in e558bec1d373050fdb26e1cb88af45ef28b69bbb
MyPads was broken when accessing it with /mypads/index.html URL

frontend/js/configuration.js
@@ -32,7 +32,7 @@ module.exports = (function () {
   var auth = require('./auth.js');
 
   var config = {};
-  config.URLS = { RAWBASE: window.location.pathname };
+  config.URLS = { RAWBASE: window.location.pathname.replace(new RegExp('index.html'), '') };
   config.URLS.BASE = config.URLS.RAWBASE + 'api';
   config.URLS.CONF = config.URLS.BASE + '/configuration';
   config.URLS.AUTH = config.URLS.BASE + '/auth';

static/js/mypads.js
@@ -3,7 +3,7 @@
 },{"./frontend/js/configuration.js":3,"./frontend/js/route.js":36}],2:[function(require,module,exports){
 module.exports=function(){var n=require("mithril"),t={};return t.admToken=function(){return localStorage.getItem("admToken")},t.isAdmin=function(){return!!t.admToken()},t.userInfo=n.prop(),t.token=function(){return localStorage.getItem("token")},t.isAuthenticated=function(){return!!t.token()},t.fn={},t.fn.xhrConfig=function(n){n.setRequestHeader("Authorization","JWT "+t.token())},t}.call(this);
 },{"mithril":46}],3:[function(require,module,exports){
-module.exports=function(){var n=require("mithril"),t=require("lodash"),e=require("./auth.js"),o={};o.URLS={RAWBASE:window.location.pathname},o.URLS.BASE=o.URLS.RAWBASE+"api",o.URLS.CONF=o.URLS.BASE+"/configuration",o.URLS.AUTH=o.URLS.BASE+"/auth",o.URLS.LOGIN=o.URLS.AUTH+"/login",o.URLS.LOGOUT=o.URLS.AUTH+"/logout",o.URLS.CHECK=o.URLS.AUTH+"/check",o.URLS.USER=o.URLS.BASE+"/user",o.URLS.USERMARK=o.URLS.USER+"mark",o.URLS.GROUP=o.URLS.BASE+"/group",o.URLS.PAD=o.URLS.BASE+"/pad",o.URLS.USERLIST=o.URLS.BASE+"/userlist",o.URLS.PASSRECOVER=o.URLS.BASE+"/passrecover",o.URLS.ACCOUNT_CONFIRMATION=o.URLS.BASE+"/accountconfirm",o.URLS.CACHECHECK=o.URLS.BASE+"/cache/check",o.SERVER=n.prop();return o.USERLANG="en",o.LANG=require("../../static/l10n/en.json"),o.updateLang=function(t){n.request({method:"GET",url:o.URLS.RAWBASE+"l10n/"+t+".json"}).then(function(n){o.USERLANG=t,o.LANG=n},function(n){console.log("updatel:"+n),o.USERLANG="en";var t=o.LANG.BACKEND.CONFIGURATION.LANG_PROBLEM+" ("+n+")";(0,require("./widgets/notification.js").error)({body:t})})},o.unauthUrl=function(){var n="";return function(t){return t&&(n=window.location.toString()),n}}(),o.init=function(S){n.request({method:"GET",url:o.URLS.CONF,data:e.isAuthenticated()?{auth_token:e.token()}:void 0}).then(function(n){o.SERVER=n.value,!n.auth&&e.isAuthenticated()&&localStorage.removeItem("token"),e.userInfo(n.user);var r=window.navigator.userLanguage||window.navigator.language;r=t.find(t.keys(o.SERVER.languages),function(n){return t.startsWith(r,n)}),r&&"en"!==r&&o.updateLang(r),S()},function(n){console.log("confinit:"+n);var t=o.LANG.BACKEND.CONFIGURATION.CANTGET+" ("+n+")";(0,require("./widgets/notification.js").error)({body:t})})},o}.call(this);
+module.exports=function(){var n=require("mithril"),e=require("lodash"),t=require("./auth.js"),o={};o.URLS={RAWBASE:window.location.pathname.replace(new RegExp("index.html"),"")},o.URLS.BASE=o.URLS.RAWBASE+"api",o.URLS.CONF=o.URLS.BASE+"/configuration",o.URLS.AUTH=o.URLS.BASE+"/auth",o.URLS.LOGIN=o.URLS.AUTH+"/login",o.URLS.LOGOUT=o.URLS.AUTH+"/logout",o.URLS.CHECK=o.URLS.AUTH+"/check",o.URLS.USER=o.URLS.BASE+"/user",o.URLS.USERMARK=o.URLS.USER+"mark",o.URLS.GROUP=o.URLS.BASE+"/group",o.URLS.PAD=o.URLS.BASE+"/pad",o.URLS.USERLIST=o.URLS.BASE+"/userlist",o.URLS.PASSRECOVER=o.URLS.BASE+"/passrecover",o.URLS.ACCOUNT_CONFIRMATION=o.URLS.BASE+"/accountconfirm",o.URLS.CACHECHECK=o.URLS.BASE+"/cache/check",o.SERVER=n.prop();return o.USERLANG="en",o.LANG=require("../../static/l10n/en.json"),o.updateLang=function(e){n.request({method:"GET",url:o.URLS.RAWBASE+"l10n/"+e+".json"}).then(function(n){o.USERLANG=e,o.LANG=n},function(n){console.log("updatel:"+n),o.USERLANG="en";var e=o.LANG.BACKEND.CONFIGURATION.LANG_PROBLEM+" ("+n+")";(0,require("./widgets/notification.js").error)({body:e})})},o.unauthUrl=function(){var n="";return function(e){return e&&(n=window.location.toString()),n}}(),o.init=function(S){n.request({method:"GET",url:o.URLS.CONF,data:t.isAuthenticated()?{auth_token:t.token()}:void 0}).then(function(n){o.SERVER=n.value,!n.auth&&t.isAuthenticated()&&localStorage.removeItem("token"),t.userInfo(n.user);var r=window.navigator.userLanguage||window.navigator.language;r=e.find(e.keys(o.SERVER.languages),function(n){return e.startsWith(r,n)}),r&&"en"!==r&&o.updateLang(r),S()},function(n){console.log("confinit:"+n);var e=o.LANG.BACKEND.CONFIGURATION.CANTGET+" ("+n+")";(0,require("./widgets/notification.js").error)({body:e})})},o}.call(this);
 },{"../../static/l10n/en.json":47,"./auth.js":2,"./widgets/notification.js":37,"lodash":45,"mithril":46}],4:[function(require,module,exports){
 module.exports=function(){var e=(require("mithril"),require("js-cookie")),a=require("../configuration.js"),t={};return t.autoExpand=function(a,d){if(!d){var s=e.get("wantExpandedPad");"string"==typeof s&&"true"===s&&t.expandIframe()}},t.isExpanded=function(){return document.querySelector("a.expand-toggle").className.indexOf("expanded")>-1},t.expandIframe=function(){var t=document.querySelectorAll("#mypads header, #mypads aside, #mypads footer"),d=document.querySelector("#mypads main.container section.col-md-9"),s=document.querySelector("#mypads main.container"),o=document.querySelector("section.pad iframe"),n=document.querySelector("a.expand-toggle i"),r=document.querySelector("a.expand-toggle");t.forEach(function(e){e.classList.add("hidden")}),d.classList.remove("col-md-9"),d.classList.add("col-md-12"),s.classList.remove("container"),s.classList.add("container-fluid"),o.style.height="80vh",n.classList.remove("glyphicon-resize-full"),n.classList.add("glyphicon-resize-small"),r.classList.add("expanded"),r.title=a.LANG.GROUP.PAD.REDUCE,e.set("wantExpandedPad","true",{expires:365,path:a.URLS.RAWBASE})},t.reduceIframe=function(t){void 0===t&&(t=!0);var d=document.querySelectorAll("#mypads header, #mypads aside, #mypads footer"),s=document.querySelector("#mypads main.container-fluid section.col-md-12"),o=document.querySelector("#mypads main.container-fluid"),n=document.querySelector("section.pad iframe"),r=document.querySelector("a.expand-toggle i"),i=document.querySelector("a.expand-toggle");d.forEach(function(e){e.classList.remove("hidden")}),s.classList.remove("col-md-12"),s.classList.add("col-md-9"),o.classList.remove("container-fluid"),o.classList.add("container"),n.style.height=null,r.classList.remove("glyphicon-resize-small"),r.classList.add("glyphicon-resize-full"),i.classList.remove("expanded"),i.title=a.LANG.GROUP.PAD.EXPAND,t&&e.set("wantExpandedPad","false",{expires:365,path:a.URLS.RAWBASE})},t}.call(this);
 },{"../configuration.js":3,"js-cookie":44,"mithril":46}],5:[function(require,module,exports){
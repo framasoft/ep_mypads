framasoft/ep_mypads

commit a0a127ab09685012f526820f266d7851c3d42457

Fix #163 (expandPad bug)

frontend/js/helpers/expandPad.js
@@ -34,15 +34,22 @@ module.exports = (function () {
   var expandPad = {};
 
   /*
-   * Expand iframe when view is initialized and the cookie says to
+   * Expand iframe when the cookie says to
    */
   expandPad.autoExpand = function(element, isInitialized) {
     if (isInitialized) return;
 
-    var wantExpandedPad = cookie.get('wantExpandedPad');
-    if (typeof wantExpandedPad === 'string' && wantExpandedPad === 'true') {
+    if (expandPad.wantExpandedPad()) {
       expandPad.expandIframe();
     }
+  }
+
+  /*
+   * Read the wantExpandedPad cookie and if it says that the user wants to expand iframe
+   */
+  expandPad.wantExpandedPad = function() {
+    var wantExpandedPad = cookie.get('wantExpandedPad');
+    return (typeof wantExpandedPad === 'string' && wantExpandedPad === 'true');
   }
 
   /*
@@ -56,23 +63,9 @@ module.exports = (function () {
    * Expand the iframe
    */
   expandPad.expandIframe = function() {
-    var toHide        = document.querySelectorAll('#mypads header, #mypads aside, #mypads footer');
-    var section9      = document.querySelector('#mypads main.container section.col-md-9');
-    var mainContainer = document.querySelector('#mypads main.container');
     var iframe        = document.querySelector('section.pad iframe');
     var aExpandI      = document.querySelector('a.expand-toggle i');
     var aExpand       = document.querySelector('a.expand-toggle');
-
-    // Hack due to browsers that does not support forEach on nodeList
-    [].forEach.call(toHide, function(element) {
-        element.classList.add('hidden');
-    });
-
-    section9.classList.remove('col-md-9');
-    section9.classList.add('col-md-12');
-
-    mainContainer.classList.remove('container');
-    mainContainer.classList.add('container-fluid');
 
     iframe.style.height = '80vh';
 
@@ -91,23 +84,9 @@ module.exports = (function () {
     if (typeof(remember) === 'undefined') {
       remember = true;
     }
-    var toShow        = document.querySelectorAll('#mypads header, #mypads aside, #mypads footer');
-    var section12     = document.querySelector('#mypads main.container-fluid section.col-md-12');
-    var mainContainer = document.querySelector('#mypads main.container-fluid');
     var iframe        = document.querySelector('section.pad iframe');
     var aExpandI      = document.querySelector('a.expand-toggle i');
     var aExpand       = document.querySelector('a.expand-toggle');
-
-    // Hack due to browsers that does not support forEach on nodeList
-    [].forEach.call(toShow, function(element) {
-        element.classList.remove('hidden');
-    });
-
-    section12.classList.remove('col-md-12');
-    section12.classList.add('col-md-9');
-
-    mainContainer.classList.remove('container-fluid');
-    mainContainer.classList.add('container');
 
     iframe.style.height = null;
 

frontend/js/modules/layout.js
@@ -142,13 +142,23 @@ module.exports = (function () {
   * - `aside` vdom content.
   */
 
+  var padViewRouteRegex = new RegExp('/mypads/group/[^/]*/pad/view/[^/]*');
   layout.view = function (main, aside, padView) {
-    var options = {};
-    if (padView) {
-      options = { config: expandPad.autoExpand };
-    };
+    var header  = 'header';
+    var footer  = 'footer.container.ombre';
+    var asideC  = 'aside.col-md-3.col-xs-12';
+    var mainC   = 'main.container.ombre';
+    var section = 'section.col-md-9.col-xs-12';
+    // If the user wants to expand pad and if we are on the view pad page
+    if (expandPad.wantExpandedPad() && m.route().match(padViewRouteRegex)) {
+      header  = header+'.hidden';
+      footer  = footer+'.hidden';
+      asideC  = asideC+'.hidden';
+      mainC   = 'main.container-fluid.ombre';
+      section = 'section.col-md-12.col-xs-12';
+    }
     return [
-      m('header', [
+      m(header, [
         m('ul.lang.container', ld.reduce(conf.SERVER.languages,
           function (memo, val, key) {
             var cls = (key === conf.USERLANG) ? 'active': '';
@@ -167,12 +177,12 @@ module.exports = (function () {
           ])
         ])
       ]),
-      m('main.container.ombre', [
-        m('section.col-md-9.col-xs-12', options, main || ''),
-        m('aside.col-md-3.col-xs-12', aside || '')
+      m(mainC, [
+        m(section, { config: expandPad.autoExpand }, main || ''),
+        m(asideC, aside || '')
       ]),
       m('section', { class: 'notification' }, notif.view(notif.controller())),
-      m('footer.container.ombre', m('p', [
+      m(footer, m('p', [
         m('span', m.trust(conf.LANG.GLOBAL.FOOTER + ' | ')),
         m('a', { href: '/admin', config: m.route }, conf.LANG.MENU.ADMIN)
       ]))

frontend/js/modules/pad-view.js
@@ -174,6 +174,7 @@ module.exports = (function () {
           target: '_blank',
           title: conf.LANG.GROUP.PAD.OPEN_TAB,
           onclick: function () {
+            expandPad.reduceIframe(false);
             c.showIframe(false);
             return true;
           }
@@ -199,11 +200,9 @@ module.exports = (function () {
     })();
     var showPass = (!c.isAdmin && isPrivate && !c.sendPass());
     if (showPass) { return view.passForm(c); }
-    var options = { class: 'group' };
-    if (isPrivate && c.sendPass()) { options.config = expandPad.autoExpand; }
     var route = '/mypads/group/' + c.gid;
     var GROUP = conf.LANG.GROUP;
-    return m('section', options, [
+    return m('section', { class: 'group' }, [
       m('.btn-group.pull-right', [
         (function () {
           var isGroupSharable = (c.group && c.group.visibility &&
@@ -269,7 +268,6 @@ module.exports = (function () {
               m('a', {
                 href: route + '/view',
                 config: m.route,
-                onclick: function() { expandPad.reduceIframe(false) },
                 title: conf.LANG.GROUP.VIEW
               }, c.group.name ),
               ')'

commit 6b95d4c9fa7537be96db7ae2e4892bc303995112

Fix bug introduced in a0a127ab (fix for #163)

frontend/js/helpers/expandPad.js
@@ -67,13 +67,20 @@ module.exports = (function () {
     var aExpandI      = document.querySelector('a.expand-toggle i');
     var aExpand       = document.querySelector('a.expand-toggle');
 
-    iframe.style.height = '80vh';
+    if (iframe) {
+      iframe.style.height = '80vh';
+    }
 
-    aExpandI.classList.remove('glyphicon-resize-full');
-    aExpandI.classList.add('glyphicon-resize-small');
+    if (aExpandI) {
+      aExpandI.classList.remove('glyphicon-resize-full');
+      aExpandI.classList.add('glyphicon-resize-small');
+    }
 
-    aExpand.classList.add('expanded');
-    aExpand.title = conf.LANG.GROUP.PAD.REDUCE;
+    if (aExpand) {
+      aExpand.classList.add('expanded');
+      aExpand.title = conf.LANG.GROUP.PAD.REDUCE;
+    }
+
     cookie.set('wantExpandedPad', 'true', { expires: 365, path: conf.URLS.RAWBASE });
   };
 
@@ -88,13 +95,20 @@ module.exports = (function () {
     var aExpandI      = document.querySelector('a.expand-toggle i');
     var aExpand       = document.querySelector('a.expand-toggle');
 
-    iframe.style.height = null;
+    if (iframe) {
+      iframe.style.height = null;
+    }
 
-    aExpandI.classList.remove('glyphicon-resize-small');
-    aExpandI.classList.add('glyphicon-resize-full');
+    if (aExpandI) {
+      aExpandI.classList.remove('glyphicon-resize-small');
+      aExpandI.classList.add('glyphicon-resize-full');
+    }
 
-    aExpand.classList.remove('expanded');
-    aExpand.title = conf.LANG.GROUP.PAD.EXPAND;
+    if (aExpand) {
+      aExpand.classList.remove('expanded');
+      aExpand.title = conf.LANG.GROUP.PAD.EXPAND;
+    }
+
     if (remember) {
       cookie.set('wantExpandedPad', 'false', { expires: 365, path: conf.URLS.RAWBASE });
     }

static/js/mypads.js
@@ -5,7 +5,7 @@ module.exports=function(){var n=require("mithril"),t={};return t.admToken=functi
 },{"mithril":46}],3:[function(require,module,exports){
 module.exports=function(){var n=require("mithril"),e=require("lodash"),t=require("./auth.js"),o={};o.URLS={RAWBASE:window.location.pathname.replace(new RegExp("index.html"),"")},o.URLS.BASE=o.URLS.RAWBASE+"api",o.URLS.CONF=o.URLS.BASE+"/configuration",o.URLS.AUTH=o.URLS.BASE+"/auth",o.URLS.LOGIN=o.URLS.AUTH+"/login",o.URLS.LOGOUT=o.URLS.AUTH+"/logout",o.URLS.CHECK=o.URLS.AUTH+"/check",o.URLS.USER=o.URLS.BASE+"/user",o.URLS.USERMARK=o.URLS.USER+"mark",o.URLS.GROUP=o.URLS.BASE+"/group",o.URLS.PAD=o.URLS.BASE+"/pad",o.URLS.USERLIST=o.URLS.BASE+"/userlist",o.URLS.PASSRECOVER=o.URLS.BASE+"/passrecover",o.URLS.ACCOUNT_CONFIRMATION=o.URLS.BASE+"/accountconfirm",o.URLS.CACHECHECK=o.URLS.BASE+"/cache/check",o.SERVER=n.prop();return o.USERLANG="en",o.LANG=require("../../static/l10n/en.json"),o.updateLang=function(e){n.request({method:"GET",url:o.URLS.RAWBASE+"l10n/"+e+".json"}).then(function(n){o.USERLANG=e,o.LANG=n},function(n){console.log("updatel:"+n),o.USERLANG="en";var e=o.LANG.BACKEND.CONFIGURATION.LANG_PROBLEM+" ("+n+")";(0,require("./widgets/notification.js").error)({body:e})})},o.unauthUrl=function(){var n="";return function(e){return e&&(n=window.location.toString()),n}}(),o.init=function(S){n.request({method:"GET",url:o.URLS.CONF,data:t.isAuthenticated()?{auth_token:t.token()}:void 0}).then(function(n){o.SERVER=n.value,!n.auth&&t.isAuthenticated()&&localStorage.removeItem("token"),t.userInfo(n.user);var r=window.navigator.userLanguage||window.navigator.language;r=e.find(e.keys(o.SERVER.languages),function(n){return e.startsWith(r,n)}),r&&"en"!==r&&o.updateLang(r),S()},function(n){console.log("confinit:"+n);var e=o.LANG.BACKEND.CONFIGURATION.CANTGET+" ("+n+")";(0,require("./widgets/notification.js").error)({body:e})})},o}.call(this);
 },{"../../static/l10n/en.json":47,"./auth.js":2,"./widgets/notification.js":37,"lodash":45,"mithril":46}],4:[function(require,module,exports){
-module.exports=function(){var e=(require("mithril"),require("js-cookie")),t=require("../configuration.js"),a={};return a.autoExpand=function(e,t){t||a.wantExpandedPad()&&a.expandIframe()},a.wantExpandedPad=function(){var t=e.get("wantExpandedPad");return"string"==typeof t&&"true"===t},a.isExpanded=function(){return document.querySelector("a.expand-toggle").className.indexOf("expanded")>-1},a.expandIframe=function(){var a=document.querySelector("section.pad iframe"),n=document.querySelector("a.expand-toggle i"),r=document.querySelector("a.expand-toggle");a.style.height="80vh",n.classList.remove("glyphicon-resize-full"),n.classList.add("glyphicon-resize-small"),r.classList.add("expanded"),r.title=t.LANG.GROUP.PAD.REDUCE,e.set("wantExpandedPad","true",{expires:365,path:t.URLS.RAWBASE})},a.reduceIframe=function(a){void 0===a&&(a=!0);var n=document.querySelector("section.pad iframe"),r=document.querySelector("a.expand-toggle i"),d=document.querySelector("a.expand-toggle");n.style.height=null,r.classList.remove("glyphicon-resize-small"),r.classList.add("glyphicon-resize-full"),d.classList.remove("expanded"),d.title=t.LANG.GROUP.PAD.EXPAND,a&&e.set("wantExpandedPad","false",{expires:365,path:t.URLS.RAWBASE})},a}.call(this);
+module.exports=function(){var e=(require("mithril"),require("js-cookie")),t=require("../configuration.js"),a={};return a.autoExpand=function(e,t){t||a.wantExpandedPad()&&a.expandIframe()},a.wantExpandedPad=function(){var t=e.get("wantExpandedPad");return"string"==typeof t&&"true"===t},a.isExpanded=function(){return document.querySelector("a.expand-toggle").className.indexOf("expanded")>-1},a.expandIframe=function(){var a=document.querySelector("section.pad iframe"),n=document.querySelector("a.expand-toggle i"),r=document.querySelector("a.expand-toggle");a&&(a.style.height="80vh"),n&&(n.classList.remove("glyphicon-resize-full"),n.classList.add("glyphicon-resize-small")),r&&(r.classList.add("expanded"),r.title=t.LANG.GROUP.PAD.REDUCE),e.set("wantExpandedPad","true",{expires:365,path:t.URLS.RAWBASE})},a.reduceIframe=function(a){void 0===a&&(a=!0);var n=document.querySelector("section.pad iframe"),r=document.querySelector("a.expand-toggle i"),d=document.querySelector("a.expand-toggle");n&&(n.style.height=null),r&&(r.classList.remove("glyphicon-resize-small"),r.classList.add("glyphicon-resize-full")),d&&(d.classList.remove("expanded"),d.title=t.LANG.GROUP.PAD.EXPAND),a&&e.set("wantExpandedPad","false",{expires:365,path:t.URLS.RAWBASE})},a}.call(this);
 },{"../configuration.js":3,"js-cookie":44,"mithril":46}],5:[function(require,module,exports){
 module.exports=function(){var i=require("mithril"),t={};return t.icon=function(t,n,a,l){var e="glyphicon mp-tooltip glyphicon-";e+=t.valid[n]()?"info-sign":"exclamation-sign";var o=t.valid[n]()?a:l;return i("i",{class:e,"data-msg":o})},t.field=function(n,a,l,e){return{label:i("label",{for:a},[l,e]),input:i("input.form-control",{name:a,value:n.data[a]()||"",oninput:t.handleField.bind(null,n)}),icon:e}},t.initFields=function(t,n){t.data={},t.valid={},n.forEach(function(n){t.data[n]=i.prop(),t.valid[n]=i.prop(!0)})},t.handleField=function(i,t,n){n||(n=t,t=void 0);var a=n.target.getAttribute("name");i.data[a](n.target.value),i.valid[a](function(){return t?n.target.checkValidity()&&t(i,n):n.target.checkValidity()}())},t.focusOnInit=function(i,t){t||i.focus()},t}.call(this);
 },{"mithril":46}],6:[function(require,module,exports){
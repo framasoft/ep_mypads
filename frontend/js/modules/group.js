b23cf9c3bb00fb1ec6e63825b9cf1a362e13f128
Luc Didry <luc@didry.org>
2018-03-16 17:24:45 +0100

    Fix #120 Remember sorting preferences

--- frontend/js/modules/group.js
+++ frontend/js/modules/group.js
@@ -38,6 +38,7 @@ module.exports = (function () {
   var layout = require('./layout.js');
   var model = require('../model/group.js');
   var padShare = require('./pad-share.js');
+  var sortingPreferences = require('../helpers/sortingPreferences.js');
 
   var group = {};
 
@@ -190,14 +191,18 @@ module.exports = (function () {
     */
 
     window.c = c;
-    c.sortField = m.prop('ctime');
-    c.sortAsc = m.prop(true);
+    c.sortField = m.prop(sortingPreferences.groupByField());
+    c.sortAsc = m.prop(sortingPreferences.groupAsc());
     c.sortBy = function (field) {
       if (c.sortField() === field) { c.sortAsc(!c.sortAsc()); }
       c.sortField(field);
       var direction = c.sortAsc() ? 'asc' : 'desc';
       c.groups = ld.transform(c.groups, function (memo, groups, type) {
         memo[type] = ld.sortByOrder(groups, field, direction);
+      });
+      sortingPreferences.updateValues({
+        groupByField: c.sortField(),
+        groupAsc: c.sortAsc()
       });
     };
 
@@ -215,6 +220,9 @@ module.exports = (function () {
 
     c.computeGroups = function () {
       c.groups = ld(model.groups()).values().sortBy(c.sortField()).value();
+      if (!c.sortAsc()) {
+        c.groups.reverse();
+      }
       var userGroups = u().bookmarks.groups;
       c.groups = ld.reduce(c.groups, function (memo, g) {
         for (var k in c.filters) {

3931b56b5395d3231523204a822394ae8f5661c3
Luc Didry <luc@didry.org>
2018-03-25 17:07:27 +0200

    Fix #51 Allow to bookmark public and private groups and pads

--- frontend/js/modules/group-view.js
+++ frontend/js/modules/group-view.js
@@ -43,7 +43,9 @@ module.exports = (function () {
   var ready = require('../helpers/ready.js');
   var sortingPreferences = require('../helpers/sortingPreferences.js');
   var filterPads = require('../helpers/filterPads.js');
+  var groupMark = require('./group-mark.js');
 
+  var u = auth.userInfo;
   var group = {};
 
   /**
@@ -457,7 +459,22 @@ module.exports = (function () {
   */
 
   view.main = function (c) {
-    var h2Elements = [ m('span', conf.LANG.GROUP.GROUP + ' ' + c.group.name) ];
+    var isBookmarked = (auth.isAuthenticated()) ? (ld.includes(u().bookmarks.groups, c.group._id)) : false;
+    var h2Elements = [ m('span', [
+      m('button.btn.btn-link.btn-lg', {
+          onclick: function (e) {
+            e.preventDefault();
+            groupMark(c.group)
+          },
+          title: (isBookmarked ? conf.LANG.GROUP.UNMARK : conf.LANG.GROUP.BOOKMARK)
+        }, [
+          m('i',
+            { class: 'glyphicon glyphicon-star' +
+              (isBookmarked ? '' : '-empty') })
+        ]
+      ),
+      conf.LANG.GROUP.GROUP + ' ' + c.group.name
+    ])];
     var shareBtn = '';
     if (c.group.visibility !== 'restricted') {
       shareBtn = m('button.btn.btn-default', {

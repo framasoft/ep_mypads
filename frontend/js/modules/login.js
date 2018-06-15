e9c8c72243e462355685f2c86619c58fd0391f82
Luc Didry <luc@framasoft.org>
2018-06-15 10:11:50 +0200

    [WIP] Fix #144

--- frontend/js/modules/login.js
+++ frontend/js/modules/login.js
@@ -76,40 +76,8 @@ module.exports = (function () {
         method: 'POST',
         url: conf.URLS.LOGIN,
         data: c.data
-      }).then(function (resp) {
-        auth.userInfo(resp.user);
-        localStorage.setItem('token', resp.token);
-
-        /*
-         * Fix pad authorship mixup
-         * See https://framagit.org/framasoft/ep_mypads/issues/148
-         */
-        var myPadsAuthorCookie  = cookies.get('token-' + resp.user.login);
-        if (myPadsAuthorCookie) {
-          // 60 days
-          cookies.set('token', myPadsAuthorCookie, { expires: 60 });
-        } else {
-          var browserAuthorCookie = cookies('token');
-          if (browserAuthorCookie) {
-            // 365 days
-            cookies.set('token-' + resp.user.login, browserAuthorCookie, { expires: 365 });
-          }
-        }
-
-        var lang = auth.userInfo().lang;
-        if (lang !== conf.USERLANG) {
-          conf.updateLang(lang);
-        }
-        document.title = ' - ' + conf.LANG.USER.AUTH.WELCOME + ' ' +
-          resp.user.login + conf.SERVER.title;
-        notif.success({ body: conf.LANG.USER.AUTH.SUCCESS });
-        var unauthUrl = conf.unauthUrl();
-        if (!ld.isEmpty(unauthUrl)) {
-          window.location = unauthUrl;
-        } else {
-          m.route('/');
-        }
-      }, function (err) {
+      }).then(login.getLogged,
+      function (err) {
         notif.error({ body: ld.result(conf.LANG, err.error) });
       });
     };
@@ -131,7 +99,7 @@ module.exports = (function () {
     login.label.attrs.class = 'col-sm-4';
     var password = user.view.field.password(c);
     var passwordBlock = [ password.input ];
-    if (conf.SERVER.authMethod !== 'ldap') {
+    if (conf.SERVER.authMethod === 'internal') {
       passwordBlock.push(
         m('p.help-block', [
           m('a', {
@@ -142,40 +110,46 @@ module.exports = (function () {
       );
     }
 
-    return m('form.form-horizontal.col-sm-8.col-sm-offset-2.well', {
-      id: 'login-form', onsubmit: c.submit }, [
-      m('div', {
-        id: 'hide-when-ready',
-        config: ready.checkLoop
-      }, conf.LANG.USER.PLEASE_WAIT),
-      m('fieldset.show-when-ready.hidden', [
-        m('legend', conf.LANG.USER.MYPADS_ACCOUNT),
-        m('.form-group', [
-          login.label,
-          m('.col-sm-7', login.input)
-        ]),
-        m('.form-group', [
-          password.label,
-          m('.col-sm-7', passwordBlock)
-        ]),
-        m('input.btn.btn-success.pull-right', {
-          form: 'login-form',
-          type: 'submit',
-          value: conf.LANG.USER.LOGIN
-        })
-      ])
-    ]);
+    if (conf.SERVER.authMethod !== 'cas') {
+      return m('form.form-horizontal.col-sm-8.col-sm-offset-2.well', {
+        id: 'login-form', onsubmit: c.submit }, [
+        m('div', {
+          id: 'hide-when-ready',
+          config: ready.checkLoop
+        }, conf.LANG.USER.PLEASE_WAIT),
+        m('fieldset.show-when-ready.hidden', [
+          m('legend', conf.LANG.USER.MYPADS_ACCOUNT),
+          m('.form-group', [
+            login.label,
+            m('.col-sm-7', login.input)
+          ]),
+          m('.form-group', [
+            password.label,
+            m('.col-sm-7', passwordBlock)
+          ]),
+          m('input.btn.btn-success.pull-right', {
+            form: 'login-form',
+            type: 'submit',
+            value: conf.LANG.USER.LOGIN
+          })
+        ])
+      ]);
+    }
   };
 
   view.main = function (c) {
     var children = [ m('span', conf.LANG.USER.FORM) ];
-    if (conf.SERVER.openRegistration && conf.SERVER.authMethod !== 'ldap') {
+    if (conf.SERVER.openRegistration && conf.SERVER.authMethod === 'internal') {
       children.push(m('a.small',
         { href: '/subscribe', config: m.route },
         conf.LANG.USER.ORSUB)
       );
     }
-    return m('section', { class: 'user' }, [
+    var opts = { class: 'user' };
+    if (conf.SERVER.authMethod === 'cas' && typeof auth.userInfo() === 'undefined') {
+      opts.config = login.casLogin;
+    }
+    return m('section', opts, [
       m('h2', children),
       view.form(c)
     ]);
@@ -183,6 +157,63 @@ module.exports = (function () {
 
   login.view = function (c) {
     return layout.view(view.main(c), user.view.aside.common(c));
+  };
+
+  login.casLogin = function (element, isInitialized) {
+    if (isInitialized) { return; }
+
+    var params = window.location.search.substring(1);
+    var ticket = params.match(new RegExp('.*&ticket=([^&]*).*'));
+    if (!ld.isNull(ticket)) {
+      m.request({
+        method: 'POST',
+        url: conf.URLS.CASLOGIN,
+        data: {ticket: ticket[1]}
+      }).then(login.getLogged,
+      function (err) {
+        notif.error({ body: ld.result(conf.LANG, err.error) });
+      });
+    } else {
+      var loc = window.location;
+      var service = encodeURIComponent(loc.origin+loc.pathname+'?/login');
+      window.location = conf.SERVER.authCasSettings.serverUrl+'/login?service='+service;
+    }
+  };
+
+  login.getLogged = function (resp) {
+    auth.userInfo(resp.user);
+    localStorage.setItem('token', resp.token);
+
+    /*
+     * Fix pad authorship mixup
+     * See https://framagit.org/framasoft/ep_mypads/issues/148
+     */
+    var myPadsAuthorCookie  = cookies.get('token-' + resp.user.login);
+    if (myPadsAuthorCookie) {
+      // 60 days
+      cookies.set('token', myPadsAuthorCookie, { expires: 60 });
+    } else {
+      var browserAuthorCookie = cookies('token');
+      if (browserAuthorCookie) {
+        // 365 days
+        cookies.set('token-' + resp.user.login, browserAuthorCookie, { expires: 365 });
+      }
+    }
+
+    var lang = auth.userInfo().lang;
+    if (lang !== conf.USERLANG) {
+      conf.updateLang(lang);
+    }
+    document.title = ' - ' + conf.LANG.USER.AUTH.WELCOME + ' ' +
+      resp.user.login + conf.SERVER.title;
+    notif.success({ body: conf.LANG.USER.AUTH.SUCCESS });
+
+    var unauthUrl = conf.unauthUrl();
+    if (!ld.isEmpty(unauthUrl)) {
+      window.location = unauthUrl;
+    } else {
+      m.route('/');
+    }
   };
   return login;
 }).call(this);
